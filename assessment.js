cc3904109f322a863749a1ddcf9d269ec5f80efc
fool-hy <123341672+fool-hy@users.noreply.github.com>
2023-01-24 09:58:18 +0900

    #2　対応しました

--- assessment.js
+++ assessment.js
@@ -52,6 +52,7 @@ assessmentButton.onclick = () => {
   tweetDivided.appendChild(script);
 };
 
+//結果一覧
 const answers = [
   '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
